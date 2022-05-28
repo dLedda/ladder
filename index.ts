2680d16a5d31ad2f0d0f018314ab5c0f032d55fa
Daniel Ledda <ledda@cip.ifi.lmu.de>
2022-05-28 13:30:22 +0200

    chore: added devdependencies, fixed exports

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import "./lib/jsxFactory";
 export type { ISubscription, IPublisher } from './lib/Publisher';
 export { Publisher } from './lib/Publisher';
-export type { default as ISubscriber, LEvent } from './lib/Subscriber';
+export type { ISubscriber, LEvent } from './lib/Subscriber';
 export { default as Rung } from './lib/Rung';
 export type { RungOptions } from './lib/Rung';
 export { default as Capsule } from './lib/Capsule';
--- lib/Subscriber.ts
+++ lib/Subscriber.ts
@@ -1,4 +1,4 @@
 export type LEvent = string;
-export default interface ISubscriber<T extends LEvent> {
+export interface ISubscriber<T extends LEvent> {
     notify(publisher: unknown, event: T): void;
 }
